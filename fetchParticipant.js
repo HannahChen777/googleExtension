58ed11c8c23b3a160316db6ceb192b7c87225f6c
Darrell_Kra <darrellwang74@gmail.com>
2022-08-18 14:54:02 +0800

    [FIX] change get status code

--- fetchParticipant.js
+++ fetchParticipant.js
@@ -1,18 +1,28 @@
+
+
 function fetchParticipantsByDOM(){
+    let knownStatus = [
+        "Declined",
+        "No response",
+        "Accepted",
+        "沒有回覆",
+        "已接受",
+        "不確定"
+    ];
+
     var abscentsFromDom = document.querySelectorAll('.m3Uzve .AE8xFb .cxdMu .SKWIhd');
+    console.log({abscentsFromDom: abscentsFromDom})
     console.log('-------abscentees-------');
     var abscentees = [];
     abscentsFromDom.forEach(function(item){
-        let isStatus = item.querySelector('.EY8ABd-OWXEXe-TAWMXe');
-        if(isStatus){  //if('isStatus == null') means yourself
-            let statusOfParticipant = isStatus.innerText;
-            if(statusOfParticipant == '沒有回覆' || statusOfParticipant == '已接受' || statusOfParticipant == '不確定'){
-                console.log(statusOfParticipant);
-                let participant = item.querySelector('.zSX24d .jKwXVe .zWGUib').innerText;
-                console.log(participant);
-                abscentees.push(participant);
-            };
-        };
+        let imgDom = item.querySelector("div.BEaVse img");
+        if(!imgDom) return;
+        let statusOfParticipant = imgDom.getAttribute("aria-label");
+        if(knownStatus.indexOf(statusOfParticipant)){
+            let participant = item.querySelector('.zSX24d .jKwXVe .zWGUib').innerText;
+            console.log(participant);
+            abscentees.push(participant);
+        }
     })
     console.log(abscentees);
     return abscentees;
