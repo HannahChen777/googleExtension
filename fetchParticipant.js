b6228ce252f492d6a3f2c29c9a92a4d2d27efdbd
HannahChen777 <hannahChen.V@gmail.com>
2022-09-06 23:11:55 +0800

    revise-repetitive-click-on-btnParticipants

--- fetchParticipant.js
+++ fetchParticipant.js
@@ -5,15 +5,24 @@
 //document.querySelector('.m3Uzve .AE8xFb .cxdMu .SKWIhd .EY8ABd-OWXEXe-TAWMXe');
 
 function clickBtnOfParticipants(){
+    console.log('clickBtnOfParticipants');
+    let isBox = document.querySelector('.WUFI9b');  // to get the whole box of btnParticipants
+    
+    if(!isBox.classList.contains('qdulke'));  // to judge if the box is opened  //true: closed; false:open
+        return;
+    //need to fix(null)
+
     let btns = document.getElementsByClassName('VfPpkd-Bz112c-LgbsSe yHy1rc eT1oJ JsuyRc boDUxc') || '';
     if(!btns)
         return;
+
     btns[1].click();
     return true;
     
 }
 
 function fetchParticipantsByDOM(){
+    console.log('fetchParticipantsByDOM');
     let knownStatus = [
         "No response",
         "Accepted",
